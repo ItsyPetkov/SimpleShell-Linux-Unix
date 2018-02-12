3ea4c04c8b63bbfa654206c1a1e9bdb159f343fc
Shivam Khattar <shivam.khattar.2016@uni.strath.ac.uk>
2018-02-12 18:40:49 +0000

    Phase 3&4 Complete

--- shell.c
+++ shell.c
@@ -8,34 +8,28 @@
 
 #define BUFFER_SIZE 512
 
-void displayPrompt();
-void readInput();
 void exitCheck(char *input);
 void parseInput(char *input);
-void externalCommandexec(char * tokens[50]);
-void runShell(char *input);
+void externalCommandexec(char * tokens[]);
+void runShell();
+void changeDirectory(char * tokens[]);
+void setPath(char *path) ;
+void getPath();
+void restorePath();
+void startShell();
+void setHome(); 
+void commandCheck(char * tokens[]);
+void changeDirectory(char * tokens[]);
 
 int terminate = 0;
+char* path;
+const char* home;
 extern char **environ;
 
 int main(void){
-	displayPrompt();
+	startShell();
+	runShell();
 	return 0;
-}
-
-void displayPrompt(){
-	while (terminate == 0){
-		printf(">");
-		readInput();
-	}		
-}
-
-void readInput(){
-	char input[BUFFER_SIZE];
-    	if (fgets(input,BUFFER_SIZE,stdin)==NULL){
-        	exit(0);
-    	}
-	runShell(input);
 }
 
 void exitCheck(char *input){
@@ -49,28 +43,41 @@ void parseInput(char *input){
 	char *token;
 	char * tokenarray[50];
 	token=strtok(input," |><&\t\n;");
+
 	while(token != NULL){
 		tokenarray[position]=token;
 		token = strtok(NULL," |><&\t\n;");
 		position++;
 	}
+
 	for(int i=position;i<50;i++){
 		tokenarray[i]=NULL;
 	}
-	externalCommandexec(tokenarray);	
+
+	commandCheck(tokenarray);
+	
 }
 
-void runShell(char *input){
-	exitCheck(input);
-	if(input[0] != '\n'){
-		parseInput(input);
+void runShell(){
+	char input[BUFFER_SIZE];
+	while (terminate == 0){
+		printf(">");
+		memset(input,'\0',BUFFER_SIZE);
+		if (fgets(input,BUFFER_SIZE,stdin)==NULL){
+			printf("\n");
+        		exit(0);
+    		}
+		exitCheck(input);
+		if(input[0] != '\n'){
+			parseInput(input);
+		}
 	}
+	
 }
 
-void externalCommandexec(char * tokens[50]){
+void externalCommandexec(char * tokens[]){
 	pid_t  pid;
  	pid = fork();
-	
 	if (pid < 0){
 		printf("ERROR!! Child Process could not be created\n");
 	}
@@ -85,4 +92,69 @@ void externalCommandexec(char * tokens[50]){
     		
 }
 
+void getPath() {
+    char *currpath = getenv("PATH");
+    printf("PATH : %s\n", currpath);
+    
+}
+
+void setPath(char *path) {
+    if (setenv("PATH", path, 1) == 0) {
+        printf("PATH : %s\n", getenv("PATH"));
+    } else {
+        puts("setenv() error.");
+    }
+}
+
+void restorePath() {
+    setPath(path);
+}
+
+void setHome() {
+    char directory[512];
+    path = getenv("PATH");
+    home = getenv("HOME");
+    
+    
+    if (chdir(home) == 0) {
+        printf("HOME : %s\n", getcwd(directory, sizeof(directory)));
+    } else {
+        printf("chdir() error.\n");
+    }
+
+}
+
+void startShell(){
+	setHome();
+	//setPath(path);
+	//restorePath();
+}
+
+void commandCheck(char * tokens[]){
+	
+	if(strcmp(tokens[0],"getpath")==0){
+		getPath();
+	}
+
+	else if(strcmp(tokens[0],"setpath")==0){
+		setPath(tokens[1]);
+	}
+
+	else if(strcmp(tokens[0],"cd")==0){
+		changeDirectory(tokens);
+	}
+	
+	else{
+		externalCommandexec(tokens);	
+	}
+}
+
+void changeDirectory(char * tokens[]){
+	if(tokens[1]==NULL){
+		chdir(home);
+	}else if(chdir(tokens[1])==0){
+		char directory[512];
+		printf("Directory : %s\n", getcwd(directory, sizeof(directory)));
+	}
+}
 
--- test.c
+++ test.c
@@ -13,12 +13,17 @@ void parseInput(char *input);
 void externalCommandexec(char * tokens[]);
 void runShell();
 void changeDirectory(char * tokens[]);
-void setdefaultPath();
-void getdefaultPath();
+void setPath(char *path) ;
+void getPath();
+void restorePath();
 void startShell();
+void setHome(); 
+void commandCheck(char * tokens[]);
+void changeDirectory(char * tokens[]);
 
 int terminate = 0;
-char* PATH;
+char* path;
+const char* home;
 extern char **environ;
 
 int main(void){
@@ -29,7 +34,6 @@ int main(void){
 
 void exitCheck(char *input){
 	if(strncmp(input, "exit", 4)==0){
-		setdefaultPath();
 		exit(0);
 	}
 }
@@ -39,15 +43,19 @@ void parseInput(char *input){
 	char *token;
 	char * tokenarray[50];
 	token=strtok(input," |><&\t\n;");
+
 	while(token != NULL){
 		tokenarray[position]=token;
 		token = strtok(NULL," |><&\t\n;");
 		position++;
 	}
+
 	for(int i=position;i<50;i++){
 		tokenarray[i]=NULL;
 	}
-	externalCommandexec(tokenarray);	
+
+	commandCheck(tokenarray);
+	
 }
 
 void runShell(){
@@ -57,7 +65,6 @@ void runShell(){
 		memset(input,'\0',BUFFER_SIZE);
 		if (fgets(input,BUFFER_SIZE,stdin)==NULL){
 			printf("\n");
-			setdefaultPath();
         		exit(0);
     		}
 		exitCheck(input);
@@ -85,25 +92,69 @@ void externalCommandexec(char * tokens[]){
     		
 }
 
-void setdefaultPath(){
-	setenv("PATH",PATH,1);
-	chdir(PATH);
+void getPath() {
+    char *currpath = getenv("PATH");
+    printf("PATH : %s\n", currpath);
+    
 }
 
-void getdefaultPath(){
-	PATH=getenv("PATH");
+void setPath(char *path) {
+    if (setenv("PATH", path, 1) == 0) {
+        printf("PATH : %s\n", getenv("PATH"));
+    } else {
+        puts("setenv() error.");
+    }
+}
+
+void restorePath() {
+    setPath(path);
+}
+
+void setHome() {
+    char directory[512];
+    path = getenv("PATH");
+    home = getenv("HOME");
+    
+    
+    if (chdir(home) == 0) {
+        printf("HOME : %s\n", getcwd(directory, sizeof(directory)));
+    } else {
+        printf("chdir() error.\n");
+    }
+
 }
 
 void startShell(){
-	getdefaultPath();
-	chdir(getenv("HOME"));
+	setHome();
+	//setPath(path);
+	//restorePath();
+}
+
+void commandCheck(char * tokens[]){
+	
+	if(strcmp(tokens[0],"getpath")==0){
+		getPath();
+	}
+
+	else if(strcmp(tokens[0],"setpath")==0){
+		setPath(tokens[1]);
+	}
+
+	else if(strcmp(tokens[0],"cd")==0){
+		changeDirectory(tokens);
+	}
+	
+	else{
+		externalCommandexec(tokens);	
+	}
 }
 
 void changeDirectory(char * tokens[]){
-
-	
+	if(tokens[1]==NULL){
+		chdir(home);
+	}else if(chdir(tokens[1])==0){
+		char directory[512];
+		printf("Directory : %s\n", getcwd(directory, sizeof(directory)));
+	}
 }
-
-
-
 
